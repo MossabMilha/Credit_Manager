e03b9dad081452823059999548deb0f725f506be
MMILHA <98252587+MossabMilha@users.noreply.github.com>
2024-11-05 00:04:39 +0100

    M working in info button not yet finesh in progress

--- main.c
+++ main.c
@@ -36,6 +36,10 @@ char check_first_name(const char *first_name,const char *last_name) {
 
 
 }
+/*
+void check_first_name(const ) {
+
+}*/
 void check_password(const gchar *password,const gchar *confirm_password, const gchar *first_name, const gchar *last_name, GtkWindow *parent) {
     const int is_Same =strncmp(password ,confirm_password,strlen(password));
     char message[1024] = "";
@@ -94,14 +98,23 @@ void check_password(const gchar *password,const gchar *confirm_password, const g
 void check_SignUp(GtkWidget *widget, gpointer user_data) {
     // Retrieve the signup window from user_data
     GtkWidget *signup_window=GTK_WIDGET(user_data);
+    GtkWidget *signup_fix = g_object_get_data(G_OBJECT(signup_window),"signup_fix");
     int is_feild_empty = 0;
 
     //Retrieve the first_name information
     GtkWidget *First_Name = g_object_get_data(G_OBJECT(signup_window),"First Name");
     GtkEntryBuffer *first_name_buffer = gtk_entry_get_buffer(GTK_ENTRY(First_Name));
     const gchar *first_name_text = gtk_entry_buffer_get_text(first_name_buffer);
-    if(strlen(first_name_text) == 0)
+
+    GtkWidget *First_Name_Button = g_object_get_data(G_OBJECT(signup_window),"First Name Image");
+
+    if(strlen(first_name_text) == 0) {
         is_feild_empty = 1;
+        gtk_widget_set_visible(First_Name_Button, TRUE);
+    }else {
+        gtk_widget_set_visible(First_Name_Button, FALSE);
+        //check_first_name(first_name_text);
+    }
 
 
     //Retrieve the Last_name information
@@ -191,15 +204,15 @@ static void signup_button_clicked(GtkButton *button, gpointer user_data) {
     gtk_widget_set_size_request(first_name, 250, 25);
 
     ///Crete first name image
+
     GtkWidget *first_name_button = gtk_button_new();
-
-
     GtkWidget *first_name_image = gtk_image_new_from_file("../info_red.png");
-    GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    gtk_box_append(GTK_BOX(hbox), first_name_image);
-    gtk_button_set_child(GTK_BUTTON(first_name_button), hbox);
-
+    gtk_button_set_child(GTK_BUTTON(first_name_button), first_name_image);
     gtk_fixed_put(GTK_FIXED(signup_fix), first_name_button, 310, 100);
+    gtk_widget_set_size_request(first_name_button, 20, 20);
+    gtk_widget_set_visible(first_name_button, FALSE);
+
+
     gtk_widget_set_size_request(first_name_button, 20, 20);
 
     //Last name entry
@@ -208,13 +221,15 @@ static void signup_button_clicked(GtkButton *button, gpointer user_data) {
     gtk_fixed_put(GTK_FIXED(signup_fix), Last_name, 50, 140);
     gtk_widget_set_size_request(Last_name, 250, 25);
     //Last name image
+    GtkWidget *last_name_button = gtk_button_new();
     GtkWidget *last_name_image = gtk_image_new_from_file("../info_red.png");
     GtkWidget *last_name_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    gtk_box_append(GTK_BOX(last_name_hbox), first_name_image);
-    gtk_button_set_child(GTK_BUTTON(first_name_button), last_name_hbox);
+    gtk_box_append(GTK_BOX(last_name_hbox), last_name_image);
+    gtk_button_set_child(GTK_BUTTON(last_name_button), last_name_hbox);
 
-    gtk_fixed_put(GTK_FIXED(signup_fix), first_name_button, 310, 140);
-    gtk_widget_set_size_request(first_name_button, 20, 20);
+    gtk_fixed_put(GTK_FIXED(signup_fix), last_name_button, 310, 140);
+    gtk_widget_set_size_request(last_name_button, 20, 20);
+    gtk_widget_set_visible(last_name_button, FALSE);
 
     //CIN Entry
     GtkWidget *cin =gtk_entry_new();
@@ -223,13 +238,15 @@ static void signup_button_clicked(GtkButton *button, gpointer user_data) {
     gtk_widget_set_size_request(cin, 250, 25);
 
     //CIN image
-    GtkWidget *CIN_image = gtk_image_new_from_file("../info_red.png");
-    GtkWidget *CIN_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    gtk_box_append(GTK_BOX(CIN_hbox), first_name_image);
-    gtk_button_set_child(GTK_BUTTON(first_name_button), CIN_hbox);
+    GtkWidget *cin_button = gtk_button_new();
+    GtkWidget *cin_image = gtk_image_new_from_file("../info_red.png");
+    GtkWidget *cin_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_append(GTK_BOX(cin_hbox), cin_image);
+    gtk_button_set_child(GTK_BUTTON(cin_button), cin_hbox);
 
-    gtk_fixed_put(GTK_FIXED(signup_fix), first_name_button, 310, 180);
-    gtk_widget_set_size_request(first_name_button, 20, 20);
+    gtk_fixed_put(GTK_FIXED(signup_fix), cin_button, 310, 180);
+    gtk_widget_set_size_request(cin_button, 20, 20);
+    gtk_widget_set_visible(cin_button, FALSE);
 
     //Email Entry
     GtkWidget *Email =gtk_entry_new();
@@ -237,11 +254,33 @@ static void signup_button_clicked(GtkButton *button, gpointer user_data) {
     gtk_fixed_put(GTK_FIXED(signup_fix), Email, 50, 220);
     gtk_widget_set_size_request(Email, 250, 25);
 
+    //Email image
+    GtkWidget *email_button = gtk_button_new();
+    GtkWidget *email_image = gtk_image_new_from_file("../info_red.png");
+    GtkWidget *email_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_append(GTK_BOX(email_hbox), email_image);
+    gtk_button_set_child(GTK_BUTTON(email_button), email_hbox);
+
+    gtk_fixed_put(GTK_FIXED(signup_fix), email_button, 310, 220);
+    gtk_widget_set_size_request(email_button, 20, 20);
+    gtk_widget_set_visible(email_button, FALSE);
+
     //Birthday Entry
     GtkWidget *Birthday =gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(Birthday), "Birthday DD//MM//YYY");
     gtk_fixed_put(GTK_FIXED(signup_fix), Birthday, 50, 260);
     gtk_widget_set_size_request(Birthday, 250, 25);
+
+    //Birthday image
+    GtkWidget *Birthday_button = gtk_button_new();
+    GtkWidget *Birthday_image = gtk_image_new_from_file("../info_red.png");
+    GtkWidget *Birthday_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_append(GTK_BOX(Birthday_hbox), Birthday_image);
+    gtk_button_set_child(GTK_BUTTON(Birthday_button), Birthday_hbox);
+
+    gtk_fixed_put(GTK_FIXED(signup_fix), Birthday_button, 310, 260);
+    gtk_widget_set_size_request(Birthday_button, 20, 20);
+    gtk_widget_set_visible(Birthday_button, FALSE);
 
     //Password
     GtkWidget *Password =gtk_entry_new();
@@ -251,6 +290,18 @@ static void signup_button_clicked(GtkButton *button, gpointer user_data) {
     gtk_entry_set_visibility(GTK_ENTRY(Password),FALSE);
     gtk_entry_set_invisible_char(GTK_ENTRY(Password),'*');
 
+    //Email image
+    GtkWidget *Password_button = gtk_button_new();
+    GtkWidget *Password_image = gtk_image_new_from_file("../info_red.png");
+    GtkWidget *Password_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_append(GTK_BOX(Password_hbox), Password_image);
+    gtk_button_set_child(GTK_BUTTON(Password_button), Password_hbox);
+
+    gtk_fixed_put(GTK_FIXED(signup_fix), Password_button, 310, 300);
+    gtk_widget_set_size_request(Password_button, 20, 20);
+    gtk_widget_set_visible(Password_button, FALSE);
+
+
     //Password_Confirmation
     GtkWidget *Password_Confirmation =gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(Password_Confirmation), "Confirm Password");
@@ -259,7 +310,19 @@ static void signup_button_clicked(GtkButton *button, gpointer user_data) {
     gtk_entry_set_visibility(GTK_ENTRY(Password_Confirmation),FALSE);
     gtk_entry_set_invisible_char(GTK_ENTRY(Password_Confirmation),'*');
 
+    //Confirm image
+    GtkWidget *Confirm_Password_button = gtk_button_new();
+    GtkWidget *Confirm_Password_image = gtk_image_new_from_file("../info_red.png");
+    GtkWidget *Confirm_Password_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_append(GTK_BOX(Confirm_Password_hbox), Confirm_Password_image);
+    gtk_button_set_child(GTK_BUTTON(Confirm_Password_button), Confirm_Password_hbox);
+
+    gtk_fixed_put(GTK_FIXED(signup_fix), Confirm_Password_button, 310, 340);
+    gtk_widget_set_size_request(Confirm_Password_button, 20, 20);
+    gtk_widget_set_visible(Confirm_Password_button, FALSE);
+
     // Store the entry widgets in the login window using g_object_set_data
+    g_object_set_data(G_OBJECT(signup_window), "signup_fix", signup_fix);
     g_object_set_data(G_OBJECT(signup_window), "First Name", first_name);
     g_object_set_data(G_OBJECT(signup_window), "Last Name", Last_name);
     g_object_set_data(G_OBJECT(signup_window), "CIN", cin);
@@ -267,7 +330,13 @@ static void signup_button_clicked(GtkButton *button, gpointer user_data) {
     g_object_set_data(G_OBJECT(signup_window), "Birthday DD//MM//YYY", Birthday);
     g_object_set_data(G_OBJECT(signup_window), "Password", Password);
     g_object_set_data(G_OBJECT(signup_window), "Confirm Password", Password_Confirmation);
-
+    g_object_set_data(G_OBJECT(signup_window), "First Name Image", first_name_button);
+    g_object_set_data(G_OBJECT(signup_window), "Last Name Image", last_name_button);
+    g_object_set_data(G_OBJECT(signup_window), "CIN Image", cin_button);
+    g_object_set_data(G_OBJECT(signup_window), "Email Image", email_button);
+    g_object_set_data(G_OBJECT(signup_window), "Birthday Image", Birthday_button);
+    g_object_set_data(G_OBJECT(signup_window), "Password Image", Password_button);
+    g_object_set_data(G_OBJECT(signup_window), "Confirm Password Image", Password_Confirmation);
 
 
 
